b9ecf96d90c3fd2884ec10b12489fe3e3f63b634
Andrei Ispas <andreiispas838@yahoo.com>
2023-03-20 14:11:49 +0200

    userlist done

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 import './App.css';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UserList from './pages/userList/UserList';
 
 function App() {
   return (
     <Router>
+      <Topbar />
       <div className='container'>
-        <Topbar />
         <Sidebar />
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/users'>
-            <UserList />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route exact path='/' element={<Home />}></Route>
+          <Route path='/users' element={<UserList />}></Route>
+        </Routes>
       </div>
     </Router>
   );
