11a608fd6fd18a5368d24f19798ac5bbdcd6dad7
Andrei Ispas <andreiispas838@yahoo.com>
2023-03-20 16:01:24 +0200

    user page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UserList from './pages/userList/UserList';
+import User from './pages/user/User';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Home />}></Route>
           <Route path='/users' element={<UserList />}></Route>
+          <Route path='/user/:userId' element={<User />}></Route>
         </Routes>
       </div>
     </Router>
